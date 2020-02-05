6569550233446e28c7caeefa2466832000dbaf5f
unknown <mvvenkul@gmail.com>
2020-02-05 23:53:01 +0530

    Updates

---
+++ precache-manifest.233a9eccd8e5969a9638f06e09715b74.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747f6e73fcca8bbec2f6c49254aa4ac6",
+    "url": "/leaderclasses2/index.html"
+  },
+  {
+    "revision": "c665ee7dd67ebddd6571",
+    "url": "/leaderclasses2/static/css/main.2365c2d6.chunk.css"
+  },
+  {
+    "revision": "0f3333716368c64a5aa8",
+    "url": "/leaderclasses2/static/js/2.9815d9ff.chunk.js"
+  },
+  {
+    "revision": "33680f10ac176f8b277c53fed91c1f3b",
+    "url": "/leaderclasses2/static/js/2.9815d9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c665ee7dd67ebddd6571",
+    "url": "/leaderclasses2/static/js/main.40cc2ff3.chunk.js"
+  },
+  {
+    "revision": "a9d0c88355681937f45a6ebfeeab0d83",
+    "url": "/leaderclasses2/static/js/main.40cc2ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3515efb6943327229011",
+    "url": "/leaderclasses2/static/js/runtime-main.5407a69b.js"
+  },
+  {
+    "revision": "987a805ec406994a26f0676f75ee4c89",
+    "url": "/leaderclasses2/static/media/2.987a805e.jpg"
+  },
+  {
+    "revision": "ab188a59f5fbdfddc18d555bb8c32956",
+    "url": "/leaderclasses2/static/media/3.ab188a59.jpg"
+  },
+  {
+    "revision": "e9fe38e867f2b01c38526ca8cfe6c085",
+    "url": "/leaderclasses2/static/media/4.e9fe38e8.jpg"
+  },
+  {
+    "revision": "15fe24bac7ee0fd89d8f38a8f6a11c49",
+    "url": "/leaderclasses2/static/media/5.15fe24ba.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/leaderclasses2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/leaderclasses2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/leaderclasses2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/leaderclasses2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/leaderclasses2/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/leaderclasses2/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/leaderclasses2/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/leaderclasses2/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/leaderclasses2/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/leaderclasses2/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/leaderclasses2/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/leaderclasses2/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/leaderclasses2/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/leaderclasses2/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/leaderclasses2/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
